38cc8d5092d6e6c1701ae8757db4653f65df801f
Harv <85876365+harvinder52@users.noreply.github.com>
2023-09-03 19:37:59 +0530

    added mediaquery

--- src/components/canvas/Computers.jsx
+++ src/components/canvas/Computers.jsx
@@ -28,6 +28,24 @@ const Computers = () => {
 };
 
 const ComputersCanvas = () => {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const mediaQuery = window.matchMedia("(max-width: 500px)");
+
+    setIsMobile(mediaQuery.matches);
+
+    const handleMediaQueryChange = (event) => {
+      setIsMobile(event.matches);
+    };
+
+    mediaQuery.addEventListener("change", handleMediaQueryChange);
+
+    return () => {
+      mediaQuery.removeEventListener("change", handleMediaQueryChange);
+    };
+  }, []);
+
   return (
     <Canvas
       frameloop="demand"
